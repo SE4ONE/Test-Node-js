bf88d5833bcb3ccd81b5a74ed3852e3bf3b25be8
SE40NE <nung3634@gmail.com>
2022-12-05 17:29:33 +0700

    update

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const comu = require('chalk');
 const morgan = require('morgan');
 const debug = require('debug')('app');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
+
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
 
 app.get("/", (req, res) => {
 
