54c02c888ccd4167f4f792a606dab0c319986ccb
SE40NE <nung3634@gmail.com>
2022-12-06 18:32:27 +0700

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const comu = require('chalk');
 const morgan = require('morgan');
 const debug = require('debug')('app');
 const path = require('path');
-
+const productRounter = express.Router();
 const app = express();
 const PORT = process.env.PORT;
 
@@ -14,9 +14,18 @@ app.use(express.static(path.join(__dirname, "/public/")));
 app.set("views", "./src/views");
 app.set("view engine", "ejs")
 
+
+productRounter.route("/").get((req,res) =>{
+    res.send("hello world")
+})
+productRounter.route("/1").get((req,res) =>{
+    res.send("hello world")
+})
+app.use("/products",productRounter)
+
 app.get("/", (req, res) => {
 
-    res.render('index', {username: 'Nuengza55+',customers:["Nueng","tong","M","amm","sai"] });
+    res.render('index', {username: 'Nuengza55+',customers:["Nueng","tong","M111","amm","sai"] });
 
 })
 app.listen(PORT, () => {
